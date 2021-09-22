bc926dbff76110cc407ae9f4bc1159c52281a550
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-22 12:26:22 -0700

    add reducers/index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 import App from './src/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ src/components/NewMarkerForm.js
@@ -0,0 +1,1 @@
+import { useFirestore } from 'react-redux-firebase'
