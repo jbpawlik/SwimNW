7b79e50675e703b1fc3fe0b174faf4d2dcc3d4c0
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-23 14:20:14 -0700

    WIP: retrieve map markers from firebase

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
+import MapView, {Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+// import Marker from './Marker';
+import Markers from './Markers';
 
 export default class Map extends Component {
+
+  
+  state = {
+    markers: 
+    [
+      {
+        coordinate: {
+          latitude: 45.5,
+          longitude: -122.675
+        },
+        title: 'Test',
+        description: 'Test'
+      }
+
+    ], 
+    region: {
+      latitude: 45.5,
+      longitude: -122.675,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1}
+  }
 
   render() {
     return (
@@ -17,24 +39,31 @@ export default class Map extends Component {
          longitude: -122.675,
          latitudeDelta: 0.1,
          longitudeDelta: 0.1}}
-      />
+         customMapStyle={mapStyle}
+         >
+           {this.state.markers.map((marker, index) => (
+           <Marker
+           key={index}
+          coordinate={marker.coordinate}
+          title={marker.name}
+          />
+           ))}
+      </MapView>
     );
   }
 }
 
-// const styles = {
-//   snazzy : [
-//     {
-//         "featureType": "administrative.country",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "visibility": "simplified"
-//             },
-//             {
-//                 "hue": "#ff0000"
-//             }
-//         ]
-//     }
-// ]
-// }
+const mapStyle = [
+    {
+        "featureType": "administrative.country",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    }
+]
--- src/components/Markers.js
+++ src/components/Markers.js
@@ -4,35 +4,46 @@ import PropTypes from "prop-types";
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 
-function Markers(props){
+
+export default function Markers() {
   useFirestoreConnect([
     { collection: 'markers' }
-  ]);
+    ]);
   const markers = useSelector(state => state.firestore.ordered.markers);
-  if (isLoaded(markers)) {
-    return (
-      <React.Fragment>
-        <hr/>
-        {markers.map((marker) => {
-          return <Marker
-            whenMarkerClicked = { props.onMarkerSelection }
-            name={marker.name}
-            id={marker.id}
-            key={marker.id}/>
-        })}
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <React.Fragment>
-        <h3>Loading...</h3>
-      </React.Fragment>
-    )
-  }
+  return markers;
 }
 
-Markers.propTypes = {
-  onMarkerSelection: PropTypes.func
-};
+// function Markers(props){
 
-export default Markers;
+//   useFirestoreConnect([
+//     { collection: 'markers' }
+//   ]);
+
+//   const markers = useSelector(state => state.firestore.ordered.markers);
+
+//   if (isLoaded(markers)) {
+//     return (
+//       <React.Fragment>
+//         {markers.map((marker) => {
+//           return <Marker
+//             whenMarkerClicked = { props.onMarkerSelection }
+//             name={marker.name}
+//             id={marker.id}
+//             key={marker.id}/>
+//         })}
+//       </React.Fragment>
+//     );
+//   } else {
+//     return (
+//       <React.Fragment>
+//         <h3>Loading...</h3>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// Markers.propTypes = {
+//   onMarkerSelection: PropTypes.func
+// };
+
+// export default Markers;
