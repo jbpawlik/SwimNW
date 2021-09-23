ef501afaffe3b7477d4e31cffdabe3fa69544dab
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-23 07:52:06 -0700

    add user signup

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,91 +1,12 @@
 import React from "react";
-import firebase from "firebase/app";
-import {Text, StyleSheet, View} from 'react-native';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import UserForm from "../components/UserForm";
-import FormTextInput from '../components/FormTextInput';
-import Button from '../components/Button';
+import Signin from "../components/Signin";
+import Signup from "../components/Signup";
 
-function Account(){  
-  function doSignIn(event) {
-    event.preventDefault();
-    const email = event.target.signinEmail.value;
-    const password = event.target.signinPassword.value;
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-      console.log("Successfully signed in!");
-    }).catch(function(error) {
-      console.log(error.message);
-    });
-  }
- 
-  
+export default function Account() {
   return (
     <React.Fragment>
-      <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Text style={{ color: '#223e4b', fontSize: 20, marginBottom: 16 }}>
-        Login
-      </Text>
-      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-        <FormTextInput
-          icon='mail'
-          placeholder='Enter your email'
-          autoCapitalize='none'
-          autoCompleteType='email'
-          keyboardType='email-address'
-          keyboardAppearance='dark'
-          returnKeyType='next'
-          returnKeyLabel='next'
-        />
-      </View>
-      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-        <FormTextInput
-          icon='key'
-          placeholder='Enter your password'
-          secureTextEntry
-          autoCompleteType='password'
-          autoCapitalize='none'
-          keyboardAppearance='dark'
-          returnKeyType='go'
-          returnKeyLabel='go'
-        />
-      </View>
-      <Button label='Login' onPress={() => true} />
-        {/* <UserForm/> */}
-      </View>
-      {/* <h1>Sign up</h1>
-      <Form onSubmit={doSignUp}>
-        <input
-          type='text'
-          name='email'
-          placeholder='email' />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password' />
-        <button type='submit'>Sign up</button>
-      </form>
-      <h1>Sign In</h1>
-      <form onSubmit={doSignIn}>
-        <input
-          type='text'
-          name='signinEmail'
-          placeholder='email' />
-        <input
-          type='password'
-          name='signinPassword'
-          placeholder='Password' />
-        <button type='submit'>Sign in</button>
-      </form>
-      <h1>Sign Out</h1>
-      <Button onClick={doSignOut}>Sign outr</Button> */}
+      <Signup/>
+      <Signin/>
     </React.Fragment>
   )
 }
-export default Account
