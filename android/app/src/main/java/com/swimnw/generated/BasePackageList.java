95ba0c9939e59ab6c9472c14381f30ae3a8f272c
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-10-01 16:59:01 -0700

    add ImagePicker packages

--- android/app/src/main/java/com/swimnw/generated/BasePackageList.java
+++ android/app/src/main/java/com/swimnw/generated/BasePackageList.java
@@ -8,11 +8,14 @@ public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
         new expo.modules.application.ApplicationPackage(),
+        new expo.modules.camera.CameraPackage(),
+        new expo.modules.clipboard.ClipboardPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage()
