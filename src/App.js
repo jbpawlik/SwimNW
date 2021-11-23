b319a9c1736506cc35ac3d46fae25157a7bddce7
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-11-23 01:35:59 -0800

    remove LogBox warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Map from './components/Map'
@@ -6,9 +5,10 @@ import DrawerMenu from './components/DrawerMenu';
 import firebase from '../src/firebase';
 import 'react-native-gesture-handler';
 
+// Instead of vvv, go to node_modules/react-native/Libraries/Core/JSTimers.js and update to const MAX_TIMER_DURATION_MS = 10000 * 1000;
 // Ignore timer warning in re: Firebase; fundamental incompatibility with React Native; placeholder solution while debugging (remove on iOS)
-import { LogBox } from 'react-native';
-LogBox.ignoreLogs(['Setting a timer for a long period of time'])
+// import { LogBox } from 'react-native';
+// LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
