f07b4667f82a968225c5e0002c6bf8831cfc4328
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-11-23 01:02:13 -0800

    fix firebase .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,25 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore';
-// import {REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_AUTH_DOMAIN, REACT_APP_FIREBASE_DATABASE_URL, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_SENDER_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_APP_ID} from '@env'
-
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID 
-// }
-
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_DATABASE_URL,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtp7rpL40bUmkI-GWk771_xF1SltIRtz0",
-  authDomain: "swimnw.firebaseapp.com" ,
-  databaseURL: "https://SwimNW.firebaseio.com",
-  projectId: "swimnw",
-  storageBucket: "gs://swimnw.appspot.com",
-  messagingSenderId: "638140442903",
-  appId: "1:638140442903:web:8b7e88477773bc04166f52"
-}
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
 
