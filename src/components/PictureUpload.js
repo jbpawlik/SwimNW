6f82381b29e2b23d827433d425c2086f7c5bf762
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-01-05 10:59:11 -0800

    prune redundant / obsolete code

--- src/components/PictureUpload.js
+++ src/components/PictureUpload.js
@@ -9,7 +9,6 @@ import 'firebase/storage';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const storageRef = firebase.storage().ref();
-const imageRef = storageRef.child('photo.jpg');
 
 // const photoRef = storageRef.child('photo.png');
 // const imagesRef = storageRef.child('images/photo.png');
@@ -24,15 +23,12 @@ export default function PictureUpload({selectedMarker, hideUploadPicture}) {
   const imageRef = storageRef.child(selectedMarker + '.jpg');
   const user = firebase.auth().currentUser;
 
-  // const { pickedImagePath } = route.params
-  // console.log(pickedImagePath)
-
-  // useEffect(() => {
-  //   const URL = imageRef.getDownloadURL()
-  //   return () => {
-  //     console.log(URL)
-  //   }
-  // }, [])
+  useEffect(() => {
+    const URL = imageRef.getDownloadURL()
+    return () => {
+      console.log(URL)
+    }
+  }, [])
 
   const openCamera = async () => {
     const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
@@ -48,14 +44,6 @@ export default function PictureUpload({selectedMarker, hideUploadPicture}) {
       setPickedImagePath(result.uri);
     }
   }
-
-  // // Haven't saved yet - don't need to
-  // const uploadBlob = (file) => {
-  //   savedImagesRef.put(file).then((snapshot) => {
-  //     console.log('Uploaded a blob or file!');
-  //   });
-  //   // [END storage_upload_blob]
-  // }
 
   const onBackButtonPress = () => {
     // navigation.navigate("MapScreen");
@@ -263,56 +251,6 @@ const uploadImageAsync = async (uri) => {
   return fetch(apiUrl, options);
 }
 
-//     return (
-//       <View style={styles.view}>
-//         <Entypo
-//           name='camera'
-//           size={120}
-//           onPress={() => takePhoto()} title="Take a photo"
-//         />
-//         {/* <AntDesign
-//           name='addfolder'
-//           size={120}
-//           onPress={this._pickImage}
-//         /> */}
-//         <AntDesign
-//           name='back'
-//           size={120}
-//           onPress={() => onBackButtonPress()}
-//         />
-//         {maybeRenderImage()}
-//         {maybeRenderUploadingOverlay()}
-//         <StatusBar barStyle="default" />
-//       </View>
-//     );
-//   }
-
-  
-
-  //   return (
-  //     <View style={styles.view}>
-  //       <Entypo
-  //         name='camera'
-  //         size={120}
-  //         onPress={() => takePhoto()} title="Take a photo"
-  //       />
-  //       {/* <AntDesign
-  //         name='addfolder'
-  //         size={120}
-  //         onPress={this._pickImage}
-  //       /> */}
-  //       <AntDesign
-  //         name='back'
-  //         size={120}
-  //         onPress={() => onBackButtonPress()}
-  //       />
-  //       {maybeRenderImage()}
-  //       {maybeRenderUploadingOverlay()}
-  //       <StatusBar barStyle="default" />
-  //     </View>
-  //   );
-  // }
-
   return (
     <View style={styles.screen}>
       { !pickedImagePath ?
@@ -332,8 +270,6 @@ const uploadImageAsync = async (uri) => {
             size={180}
             onPress={() => onBackButtonPress()}
           />
-          {/* {maybeRenderImage()}
-          {maybeRenderUploadingOverlay()} */}
         </View>
         :
         <View style={styles.imageContainer}>
@@ -383,254 +319,4 @@ const styles = StyleSheet.create({
     height: 300,
     resizeMode: 'cover'
   },
-  // exampleText: {
-  //   fontSize: 20,
-  //   marginBottom: 20,
-  //   marginHorizontal: 15,
-  //   textAlign: 'center',
-  // },
-  // maybeRenderUploading: {
-  //   alignItems: 'center',
-  //   backgroundColor: 'rgba(0,0,0,0.4)',
-  //   justifyContent: 'center',
-  // },
-  // maybeRenderContainer: {
-  //   borderRadius: 3,
-  //   elevation: 2,
-  //   marginTop: 30,
-  //   shadowColor: 'rgba(0,0,0,1)',
-  //   shadowOpacity: 0.2,
-  //   shadowOffset: {
-  //     height: 4,
-  //     width: 4,
-  //   },
-  //   shadowRadius: 5,
-  //   width: 250,
-  // },
-  // maybeRenderImageContainer: {
-  //   borderTopLeftRadius: 3,
-  //   borderTopRightRadius: 3,
-  //   overflow: 'hidden',
-  // },
-  // maybeRenderImage: {
-  //   height: 250,
-  //   width: 250,
-  // },
-  // maybeRenderImageText: {
-  //   paddingHorizontal: 10,
-  //   paddingVertical: 10,
-  // },
-  // view: {
-  //   flex: 1,
-  //   alignItems: "center",
-  //   justifyContent: "space-evenly",
-  //   backgroundColor: "gold",
-  // },
 });
-
-
-// import * as ImagePicker from "expo-image-picker";
-// import * as firebase from "../firebase";
-// import React from "react";
-// import { ActivityIndicator, Dimensions, Button, Image, Share, StatusBar, StyleSheet, Text, View, LogBox, Pressable } from "react-native";
-// import { Entypo, AntDesign } from '@expo/vector-icons'
-// import * as Clipboard from "expo-clipboard";
-// import uuid from "uuid";
-// const windowWidth = Dimensions.get('window').width;
-// const windowHeight = Dimensions.get('window').height;
-// // import {getStorage, ref} from "firebase/firebase-storage"
-
-// export default class PictureUpload extends React.Component {
-//   state = {
-//     image: null,
-//     uploading: false,
-//   };
-
-//   async componentDidMount() {
-//     if (Platform.OS !== "web") {
-//       const {
-//         status,
-//       } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-//       if (status !== "granted") {
-//         alert("Sorry, we need camera roll permissions to make this work!");
-//       }
-//     }
-//   }
-
-//   render() {
-//     let { image } = this.state;
-
-//     return (  
-//       <View style={styles.view}>
-//         <Entypo 
-//           name='camera'
-//           size={120}
-//           onPress={this._takePhoto} title="Take a photo"
-//         />
-//         <AntDesign
-//           name='addfolder'
-//           size={120}
-//           onPress={this._pickImage}
-//         />
-//         {!!image && (
-//           <Text
-//             style={{
-//               fontSize: 20,
-//               marginBottom: 20,
-//               textAlign: "center",
-//               marginHorizontal: 15,
-//             }}
-//           >
-//             Example: Upload ImagePicker result
-//           </Text>
-//         )}
-//         <AntDesign 
-//           name='back'
-//           size={120}
-//           onPress={() => this.props.hideUploadPicture()}
-//         />
-//         {this._maybeRenderImage()}
-//         {this._maybeRenderUploadingOverlay()}
-//         <StatusBar barStyle="default" />
-//       </View>
-//     );
-//   }
-
-//   _maybeRenderUploadingOverlay = () => {
-//     if (this.state.uploading) {
-//       return (
-//         <View
-//           style={[
-//             StyleSheet.absoluteFill,
-//             {
-//               backgroundColor: "rgba(0,0,0,0.4)",
-//               alignItems: "center",
-//               justifyContent: "center",
-//             },
-//           ]}
-//         >
-//           <ActivityIndicator color="#fff" animating size="large" />
-//         </View>
-//       );
-//     }
-//   };
-
-//   _maybeRenderImage = () => {
-//     let { image } = this.state;
-//     if (!image) {
-//       return;
-//     }
-
-//     return (
-//       <View
-//         style={{
-//           marginTop: 30,
-//           width: 250,
-//           borderRadius: 3,
-//           elevation: 2,
-//         }}
-//       >
-//         <View
-//           style={{
-//             borderTopRightRadius: 3,
-//             borderTopLeftRadius: 3,
-//             shadowColor: "rgba(0,0,0,1)",
-//             shadowOpacity: 0.2,
-//             shadowOffset: { width: 4, height: 4 },
-//             shadowRadius: 5,
-//             overflow: "hidden",
-//           }}
-//         >
-//           <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
-//         </View>
-//         <Text
-//           onPress={this._copyToClipboard}
-//           onLongPress={this._share}
-//           style={{ paddingVertical: 10, paddingHorizontal: 10 }}
-//         >
-//           {image}
-//         </Text>
-//       </View>
-//     );
-//   };
-
-//   _share = () => {
-//     Share.share({
-//       message: this.state.image,
-//       title: "Check out this photo",
-//       url: this.state.image,
-//     });
-//   };
-
-//   _copyToClipboard = () => {
-//     Clipboard.setString(this.state.image);
-//     alert("Copied image URL to clipboard");
-//   };
-
-//   _takePhoto = async () => {
-//     let pickerResult = await ImagePicker.launchCameraAsync({
-//       allowsEditing: true,
-//       aspect: [4, 3],
-//     });
-
-//     this._handleImagePicked(pickerResult);
-//   };
-
-//   _pickImage = async () => {
-//     let pickerResult = await ImagePicker.launchImageLibraryAsync({
-//       allowsEditing: true,
-//       aspect: [4, 3],
-//     });
-
-//     this._handleImagePicked(pickerResult);
-//   };
-
-//   _handleImagePicked = async (pickerResult) => {
-//     try {
-//       this.setState({ uploading: true });
-//       if (!pickerResult.cancelled) {
-//         const uploadUrl = await uploadImageAsync(pickerResult.uri);
-//         this.setState({ image: uploadUrl });
-//         console.log(this.state)
-//       }
-//     } catch (e) {
-//       console.log(e);
-//       alert("Upload failed, sorry :(");
-//     } finally {
-//       this.setState({ uploading: false });
-//     }
-//   };
-// }
-
-// async function uploadImageAsync(uri) {
-//   const blob = await new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onload = function () {
-//       resolve(xhr.response);
-//     };
-//     xhr.onerror = function (e) {
-//       console.log(e);
-//       reject(new TypeError("Network request failed"));
-//     };
-//     xhr.responseType = "blob";
-//     xhr.open("GET", uri, true);
-//     xhr.send(null);
-//   });
-
-//   const ref = firebase.storage().ref().child(uuid.v4());
-//   const snapshot = await ref.put(blob);
-
-//   blob.close();
-
-//   return await snapshot.ref.getDownloadURL();
-// }
-
-// styles = StyleSheet.create({
-//   view: {
-//     width: windowWidth,
-//     height: windowHeight,
-//     alignItems: "center",
-//     justifyContent: "space-evenly",
-//     backgroundColor: 'beige',
-//   },
-// })
