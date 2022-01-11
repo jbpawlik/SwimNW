32c8dd4f7e078fae3a090c0281d2bf9b8c286397
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-01-10 16:27:32 -0800

    remove NewMarker.js

---
+++ src/Obsolete-ForReference/NewMarker.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import ReusableForm from "./ReusableForm";
+// import { useFirestore } from 'react-redux-firebase'
+
+
+// function NewMarker(props){
+//   const firestore = useFirestore();
+
+//   function addMarkerToFirestore(event) {
+//     event.preventDefault();
+//     props.onNewMarkerCreation();
+//     return firestore.collection('markers').add(
+//       {
+//         title: event.target.title.value,
+//         latitude: event.target.latitude.value, 
+//         longitude: event.target.longitude.value
+//       }
+//     );
+//   }
+  
+//   return (
+//     <React.Fragment>
+//       <ReusableForm/>
+//     </React.Fragment>
+//   );
+// }
+
+// NewMarker.propTypes = {
+//   onNewMarkerCreation: PropTypes.func
+// };
+
+// export default NewMarker;
